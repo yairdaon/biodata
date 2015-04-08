6aab4740fbd8e031e5269646ac1f973bb36b76dc
Yair Daon <yair.daon@gmail.com>
2015-04-08 11:10:14 -0400

    Added tests to hogsvd and changed np.dot to np.einsum

--- pkg/hogsvd.py
+++ pkg/hogsvd.py
@@ -1,4 +1,6 @@
-# a function that performs Higher Order SVD, as suggested in Ponapalli et al.
+# do HOGSVD. All you need to know to use it is:
+# data is a list of numpy arrays.
+# each numpy array has the same number of columns
 
 import numpy as np
 
@@ -7,22 +9,40 @@ def hogsvd(data):
     data is a list of numpy arrays. Each has the same number of columns, but a different 
     number of rows.
     
-    '''
+    ''' 
 
-    # ideally we would have some tests here to check whether the data is good:
-    # do the data matrices have full rank?
-    # do all matrices have same number of columns 
-
+    # initialize n to be the umber of columns each array should have
     _ , n  = data[0].shape
-    N  = len(data) # number of datasets we have
+    i = 0
 
+    # test to see if the data we get is good
+    for d in data:
+
+        # do all matrices have same number of columns 
+        assert d.shape[1] == n , "The " +str(i) + "th dataset has " + str(d.shape[1]) + " columns. It should have " + str(n) + "." 
+        
+        # do the data matrices have full rank?
+        r = np.linalg.matrix_rank(d)
+        assert r >= n , "The " + str(i) + "th dataset does not have full column rank"
+
+        # increment our counter, so we know which entry we are using
+        i = i+1
+        
+    # number of datasets we have
+    N  = len(data) 
+
+
+    # create a list of A's and its inverse
     A = []
     Ainv = []
-    # create a list of A's and its inverse
-    for D in data:
-        a = np.dot( np.transpose(D) , D) # calculate the current A  
+    for d in data:
+        a = np.einsum( " ji , jk -> ik " , d,d) 
+        #a = np.dot( np.transpose(D) , D) # calculate the current A  
         A.append( a ) # append it to the list of A's 
         Ainv.append(np.linalg.inv( a ) ) # append its inverse to the list of inverses
+
+
+
 
 
     # calcualte the matrix S - equation (2) in the paper
@@ -30,7 +50,7 @@ def hogsvd(data):
     for i in range(N):
         for j in range(i+1, N):
                                          
-            Sij = ( np.dot( A[i] , Ainv[j] ) + np.dot( A[j] , Ainv[i] ) ) /2.0
+            Sij = ( np.einsum( "ij , jk ->ik " , A[i] , Ainv[j] ) + np.einsum( "ij , jk -> ik" ,  A[j] , Ainv[i] ) ) /2.0
             S = S + Sij
 
     # finish with averaging
@@ -55,26 +75,28 @@ def hogsvd(data):
 
 
 # test it!
-n = 2
+n = 4
+datasize = 20
 # create fake data
 data =[]
-for i in range(6):
-    d = np.random.rand(i+n+1, n)
+for i in range(datasize):
+    d = np.random.rand(i+n+datasize, n)
     data.append ( d )
 
-
+# get the hogsvd 
 U , S , V = hogsvd(data)
 
-# make sure dimensions are what we expect
 for i in range(len(data)):
-    assert U[i].shape[0] == n+i+1 , str(U[0].shape[0])+ " !=  " + str( n ) 
+
+    # check that all the dimensions match
+    assert U[i].shape[0] == n+i+datasize 
     assert U[i].shape[1] == n
     assert len(S[i])  == n
     assert V.shape[0] == V.shape[1]
     assert V.shape[0] == n
 
-
+    # reconstruct the ith matrix from its components (einsum is a nice and fast way to write matrix products. I can explain it!)
     reconstructed = np.einsum( 'ij , j , kj -> ik' , U[i] , S[i] , V)
-    diff = reconstructed - data[i]
-    diff = diff*diff
-    print(str(i) + "th iteration, residual = " +str(np.sum(diff)))
+
+    # make sure it is close to the original one
+    assert np.allclose(reconstructed , data[i] ) 
